60e098fbab4fd44c8ec3ba6dfe393670ffefd9b6
LLD-Jo <josiahlawrence07@gmail.com>
2026-08-20 09:43:35 -0400

    42.00 dates

--- data/datesAndVersions.js
+++ data/datesAndVersions.js
@@ -39,7 +39,8 @@ export const SEASON_RELEASE_DATES = {
     'C6MS2': new Date(2025, 10, 1), // November 1, 2025
     'C7S1': new Date(2025, 10, 29), // November 29, 2025
     'C7S2': new Date(2026, 2, 19), // March 19, 2026
-    'C7S3': new Date(2026, 5, 6) // June 6, 2026
+    'C7S3': new Date(2026, 5, 6), // June 6, 2026
+    'C7S4': new Date(2026, 7, 20) // August 20th 2026
 };
 
 export const SEASON_UPDATE_VERSIONS = {
@@ -83,7 +84,8 @@ export const SEASON_UPDATE_VERSIONS = {
     'C6MS2': '38.00',
     'C7S1': '39.00',
     'C7S2': '40.00',
-    'C7S3': '41.00'
+    'C7S3': '41.00',
+    'C7S4': '42.00'
 };
 
 export const OG_SEASON_RELEASE_DATES = {
